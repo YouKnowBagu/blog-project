498595e3fd068ddff91256188fe3c5465cdcf06a
Sean Sampson <sean.p.sampson@gmail.com>
2016-11-28 16:53:46 -0700

    Finish HTML Signup.  Add RE checks and build initial error handling.

--- blog.py
+++ blog.py
@@ -23,7 +23,7 @@ class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-#Set root key for posts.  Keeps posts organized by specific blog if multiple blogs are made.
+#Set root key for posts to keep posts organized by specific blog, if multiple blogs are made.
 def blog_key(name = 'default'):
     return db.Key.from_path('blogs', name)
 
@@ -35,9 +35,38 @@ class NewPost(BlogHandler):
     def get(self):
         self.render('newpost.html')
 
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
 class Signup(BlogHandler):
     def get(self):
         self.render('signup-form.html')
+
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        params = dict(username = username,
+                      email = email)
+
+        if not valid_username(username):
+            params['error_username'] = "Invalid username"
+            have_error = True
+
+        if have_error:
+            self.render('signup-form.html', **params)
 
 class Welcome(BlogHandler):
     def get(self):
